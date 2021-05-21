c19808f5fe652b9309f7d60462ede18d0678f177
Pekka Rantalankila <pekka.rantalankila@gmail.com>
2021-05-21 10:44:57 +0300

    Clean up.

--- basalt_runner/scripts/calibrate_jsonl.sh
+++ basalt_runner/scripts/calibrate_jsonl.sh
@@ -12,16 +12,11 @@ target/basalt_calibrate \
   --result-path "$OUT_DIR" \
   --cam-types $CAM_TYPE $CAM_TYPE
 
-# Can the IMU numbers be removed? These are UZH-FPV values from Basalt documentation.
 target/basalt_calibrate_imu \
   --dataset-path "$1" \
   --dataset-type jsonl \
   --aprilgrid "$1"/aprilgrid.json \
-  --result-path "$OUT_DIR" \
-  --gyro-noise-std 0.05 \
-  --accel-noise-std 0.1 \
-  --gyro-bias-std 4e-5 \
-  --accel-bias-std 0.002
+  --result-path "$OUT_DIR"
 
 target/calibration_converter \
   --calib-path "$OUT_DIR"/calibration.json \
