588081a01e21297fd17897bf741206d5fefe453a
Pekka Rantalankila <pekka.rantalankila@gmail.com>
2022-09-27 10:42:56 +0300

    Change example to also draw post-processed track.

--- orbslam3_runner/docker_orbslam_run.sh
+++ orbslam3_runner/docker_orbslam_run.sh
@@ -7,4 +7,6 @@ cd vio_benchmark/orbslam3_runner/
 python3 run_benchmark.py euroc-mh-01-easy \
 	-dir /data \
 	-vocab ./ORB_SLAM3/Vocabulary \
+	-compare groundTruth,slam \
+	-metricSet full_3d_align \
 	-config ./ORB_SLAM3/Examples/Stereo-Inertial/EuRoC.yaml
